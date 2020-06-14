95e4720c180eb3563d9225cec049bc791ed73752
Wojcik Michal <m.mwojcik@outlook.com>
2020-06-14 23:55:08 +0200

    add github pages content

---
+++ docs/precache-manifest.b726f5c5aa18c2edeb866346bc0b1284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05bec9c2854192e12a1a1ae0a4b70369",
+    "url": "/index.html"
+  },
+  {
+    "revision": "246ef1c26395ff3d95fc",
+    "url": "/static/css/main.ed71ad25.chunk.css"
+  },
+  {
+    "revision": "0e7f7a17903791ac721d",
+    "url": "/static/js/2.cc775228.chunk.js"
+  },
+  {
+    "revision": "fdb2bbbbb647ba518ef54493c5cba538",
+    "url": "/static/js/2.cc775228.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "246ef1c26395ff3d95fc",
+    "url": "/static/js/main.db41a919.chunk.js"
+  },
+  {
+    "revision": "6ebd67a323ffcc1210ee",
+    "url": "/static/js/runtime-main.4cfea5c5.js"
+  }
+]);
